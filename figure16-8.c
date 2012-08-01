cb9f31502501afc679a832929008362f09d770e5
mofaph <mofaph@gmail.com>
2012-08-01 15:55:24 +0800

    figure16-8.c: 使用 get_hostname_max() 替代预编译指令 #ifndef

--- figure16-8.c
+++ figure16-8.c
@@ -51,6 +51,21 @@ serve(int sockfd)
   }
 }
 
+#if defined(_SC_HOST_NAME_MAX)
+static int
+get_hostname_max(void)
+{
+  int n = sysconf(_SC_HOST_NAME_MAX);
+  return (n < 0) ? HOST_NAME_MAX : n;
+}
+#else
+static inline int
+get_hostname_max(void)
+{
+  return HOST_NAME_MAX;
+}
+#endif
+
 int
 main(int argc, char *argv[])
 {
@@ -62,12 +77,7 @@ main(int argc, char *argv[])
   if (argc != 1)
     err_quit("usage: ruptimed");
 
-#ifdef _SC_HOST_NAME_MAX
-  n = sysconf(_SC_HOST_NAME_MAX);
-  if (n < 0)                    /* best guess */
-#endif
-    n = HOST_NAME_MAX;
-
+  n = get_hostname_max();
   host = malloc(n);
   if (host == NULL)
     err_sys("malloc error");
