cfe4ef84637510d683e439093e9f8e6cb5d0e0c1
mofaph <mofaph@gmail.com>
2012-08-02 10:46:54 +0800

    figure16-3.c: 减少冗余代码，使之变得简洁

--- figure16-3.c
+++ figure16-3.c
@@ -14,19 +14,15 @@ initserver(int type, const struct sockaddr *addr, socklen_t alen, int qlen)
 
   if ((fd = socket(addr->sa_family, type, 0)) < 0)
     return -1;
-  if (bind(fd, addr, alen) < 0) {
-    err = errno;
+  if (bind(fd, addr, alen) < 0)
     goto errout;
-  }
-  if (type == SOCK_STREAM || type == SOCK_SEQPACKET) {
-    if (listen(fd, qlen) < 0) {
-      err = errno;
+  if (type == SOCK_STREAM || type == SOCK_SEQPACKET)
+    if (listen(fd, qlen) < 0)
       goto errout;
-    }
-  }
   return fd;
 
  errout:
+  err = errno;
   close(fd);
   errno = err;
   return -1;
